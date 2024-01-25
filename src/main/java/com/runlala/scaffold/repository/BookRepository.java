1eddae9e200120b4787a6658dabca53ae5c6ea90
Guozao Meng <guozao.m@gmail.com>
2024-01-25 11:32:13 +0400

    Remove useless code

--- src/main/java/com/runlala/scaffold/repository/BookRepository.java
+++ src/main/java/com/runlala/scaffold/repository/BookRepository.java
@@ -1,10 +1,7 @@
 package com.runlala.scaffold.repository;
 
 import com.runlala.scaffold.entity.Book;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Page<Book> getAllBy(Pageable pageable);
 }
